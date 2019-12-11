38ac6b00351f28926037d176dab52d7d2a2e8dc1
EdeiltonSO <edeiltonso@gmail.com>
2019-12-11 18:24:21 -0300

    avoid heroku sleep

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+var http = require("http");
+setInterval(function() {
+  http.get("http://shmsim.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
 ReactDOM.render(<App />, document.getElementById("root"));
